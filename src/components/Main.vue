6c1ae78f1d007ea4ff03c5aee4cdfdccb6733a9a
Julian Birrer <julian.birrer@bluewin.ch>
2023-03-22 14:57:26 +0100

    added main.vue with controlls and style

---
+++ src/components/Main.vue
@@ -0,0 +1,99 @@
+<template>
+<div id="header">
+      <h1>SoundCave</h1>
+    </div>
+    <div id="content">
+      <div id="controls"></div>
+      <div id="sequencer">
+        <div class="track" id="track1">
+            <div class="edit" id="edit1">
+                <p>Edit</p>
+            </div>
+            <div class="mute" id="mute1">
+                <p>Mute</p>
+            </div>
+            <div class="play" id="play1">
+                <p>Play</p>
+            </div>
+        </div>
+        <div class="track" id="track2">
+            <div class="edit" id="edit2">
+                <p>Edit</p>
+            </div>
+            <div class="mute" id="mute2">
+                <p>Mute</p>
+            </div>
+            <div class="play" id="play2">
+                <p>Play</p>
+            </div>
+        </div>
+        <div class="track" id="track3">
+            <div class="edit" id="edit3">
+                <p>Edit</p>
+            </div>
+            <div class="mute" id="mute3">
+                <p>Mute</p>
+            </div>
+            <div class="play" id="play3">
+                <p>Play</p>
+            </div>
+        </div>
+        <div class="track" id="track4">
+            <div class="edit" id="edit4">
+                <p>Edit</p>
+            </div>
+            <div class="mute" id="mute4">
+                <p>Mute</p>
+            </div>
+            <div class="play" id="play4">
+                <p>Play</p>
+            </div>
+        </div>
+        <div class="track" id="track5">
+            <div class="edit" id="edit5">
+                <p>Edit</p>
+            </div>
+            <div class="mute" id="mute5">
+                <p>Mute</p>
+            </div>
+            <div class="play" id="play5">
+                <p>Play</p>
+            </div>
+        </div>
+        <div class="track" id="track6">
+            <div class="edit" id="edit6">
+                <p>Edit</p>
+            </div>
+            <div class="mute" id="mute6">
+                <p>Mute</p>
+            </div>
+            <div class="play" id="play6">
+                <p>Play</p>
+            </div>
+        </div>
+        <div class="track" id="track7">
+            <div class="edit" id="edit7">
+                <p>Edit</p>
+            </div>
+            <div class="mute" id="mute7">
+                <p>Mute</p>
+            </div>
+            <div class="play" id="play7">
+                <p>Play</p>
+            </div></div>
+        <div class="track" id="track8">
+            <div class="edit" id="edit8">
+                <p>Edit</p>
+            </div>
+            <div class="mute" id="mute8">
+                <p>Mute</p>
+            </div>
+            <div class="play" id="play8">
+                <p>Play</p>
+            </div></div>
+      </div>
+    </div>
+    <div id="footer">
+      <p>About</p>
+    </div>   
+</template>
