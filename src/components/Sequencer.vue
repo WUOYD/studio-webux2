21dafc9aa3155a7216236faad1bebaef74538686
WUOYD <julian.birrer@stud.hslu.ch>
2023-03-29 00:15:39 +0200

    styles changes

--- src/components/Sequencer.vue
+++ src/components/Sequencer.vue
@@ -1,174 +1,175 @@
 <template>
     <div class="content">
-      <div id="sequencer">
-        <div class="track" id="track1">
-            <div class="name" id="name1">
-                <p>Name</p>
+        <div id="sequencer">
+            <div class="track" id="track1">
+                <div class="name" id="name1">
+                    <p>Name</p>
+                </div>
+                <div class="edit" id="edit1">
+                    <p>Edit</p>
+                </div>
+                <div class="mute" id="mute1">
+                    <p>Mute</p>
+                </div>
+                <div class="play" id="play1">
+                    <p>Play</p>
+                </div>
             </div>
-            <div class="edit" id="edit1">
-                <p>Edit</p>
+            <div class="track" id="track2">
+                <div class="name" id="name2">
+                    <p>Name</p>
+                </div>
+                <div class="edit" id="edit2">
+                    <p>Edit</p>
+                </div>
+                <div class="mute" id="mute2">
+                    <p>Mute</p>
+                </div>
+                <div class="play" id="play2">
+                    <p>Play</p>
+                </div>
             </div>
-            <div class="mute" id="mute1">
-                <p>Mute</p>
+            <div class="track" id="track3">
+                <div class="name" id="name3">
+                    <p>Name</p>
+                </div>
+                <div class="edit" id="edit3">
+                    <p>Edit</p>
+                </div>
+                <div class="mute" id="mute3">
+                    <p>Mute</p>
+                </div>
+                <div class="play" id="play3">
+                    <p>Play</p>
+                </div>
             </div>
-            <div class="play" id="play1">
-                <p>Play</p>
+            <div class="track" id="track4">
+                <div class="name" id="name4">
+                    <p>Name</p>
+                </div>
+                <div class="edit" id="edit4">
+                    <p>Edit</p>
+                </div>
+                <div class="mute" id="mute4">
+                    <p>Mute</p>
+                </div>
+                <div class="play" id="play4">
+                    <p>Play</p>
+                </div>
+            </div>
+            <div class="track" id="track5">
+                <div class="name" id="name5">
+                    <p>Name</p>
+                </div>
+                <div class="edit" id="edit5">
+                    <p>Edit</p>
+                </div>
+                <div class="mute" id="mute5">
+                    <p>Mute</p>
+                </div>
+                <div class="play" id="play5">
+                    <p>Play</p>
+                </div>
+            </div>
+            <div class="track" id="track6">
+                <div class="name" id="name6">
+                    <p>Name</p>
+                </div>
+                <div class="edit" id="edit6">
+                    <p>Edit</p>
+                </div>
+                <div class="mute" id="mute6">
+                    <p>Mute</p>
+                </div>
+                <div class="play" id="play6">
+                    <p>Play</p>
+                </div>
+            </div>
+            <div class="track" id="track7">
+                <div class="name" id="name7">
+                    <p>Name</p>
+                </div>
+                <div class="edit" id="edit7">
+                    <p>Edit</p>
+                </div>
+                <div class="mute" id="mute7">
+                    <p>Mute</p>
+                </div>
+                <div class="play" id="play7">
+                    <p>Play</p>
+                </div>
+            </div>
+            <div class="track" id="track8">
+                <div class="name" id="name8">
+                    <p>Name</p>
+                </div>
+                <div class="edit" id="edit8">
+                    <p>Edit</p>
+                </div>
+                <div class="mute" id="mute8">
+                    <p>Mute</p>
+                </div>
+                <div class="play" id="play8">
+                    <p>Play</p>
+                </div>
             </div>
         </div>
-        <div class="track" id="track2">
-            <div class="name" id="name2">
-                <p>Name</p>
-            </div>
-            <div class="edit" id="edit2">
-                <p>Edit</p>
-            </div>
-            <div class="mute" id="mute2">
-                <p>Mute</p>
-            </div>
-            <div class="play" id="play2">
-                <p>Play</p>
-            </div>
-        </div>
-        <div class="track" id="track3">
-            <div class="name" id="name3">
-                <p>Name</p>
-            </div>
-            <div class="edit" id="edit3">
-                <p>Edit</p>
-            </div>
-            <div class="mute" id="mute3">
-                <p>Mute</p>
-            </div>
-            <div class="play" id="play3">
-                <p>Play</p>
-            </div>
-        </div>
-        <div class="track" id="track4">
-            <div class="name" id="name4">
-                <p>Name</p>
-            </div>
-            <div class="edit" id="edit4">
-                <p>Edit</p>
-            </div>
-            <div class="mute" id="mute4">
-                <p>Mute</p>
-            </div>
-            <div class="play" id="play4">
-                <p>Play</p>
-            </div>
-        </div>
-        <div class="track" id="track5">
-            <div class="name" id="name5">
-                <p>Name</p>
-            </div>
-            <div class="edit" id="edit5">
-                <p>Edit</p>
-            </div>
-            <div class="mute" id="mute5">
-                <p>Mute</p>
-            </div>
-            <div class="play" id="play5">
-                <p>Play</p>
-            </div>
-        </div>
-        <div class="track" id="track6">
-            <div class="name" id="name6">
-                <p>Name</p>
-            </div>
-            <div class="edit" id="edit6">
-                <p>Edit</p>
-            </div>
-            <div class="mute" id="mute6">
-                <p>Mute</p>
-            </div>
-            <div class="play" id="play6">
-                <p>Play</p>
-            </div>
-        </div>
-        <div class="track" id="track7">
-            <div class="name" id="name7">
-                <p>Name</p>
-            </div>
-            <div class="edit" id="edit7">
-                <p>Edit</p>
-            </div>
-            <div class="mute" id="mute7">
-                <p>Mute</p>
-            </div>
-            <div class="play" id="play7">
-                <p>Play</p>
-            </div></div>
-        <div class="track" id="track8">
-            <div class="name" id="name8">
-                <p>Name</p>
-            </div>
-            <div class="edit" id="edit8">
-                <p>Edit</p>
-            </div>
-            <div class="mute" id="mute8">
-                <p>Mute</p>
-            </div>
-            <div class="play" id="play8">
-                <p>Play</p>
-            </div></div>
-      </div>
-    </div>  
+    </div>
 </template>
 
 <style scoped>
-
-#sequencer{
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
-  height: 90%;
+#sequencer {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    height: 90%;
 }
 
-.track{ 
-  width: calc(70% / 2 );
-  height: auto;
-  margin: auto; 
-  border: 1px solid #fff;
-  justify-items: stretch;
-  display: grid;
-  box-sizing: border-box;
+.track {
+    width: calc(70% / 2);
+    height: auto;
+    margin: auto;
+    border: 1px solid #fff;
+    justify-items: stretch;
+    display: grid;
+    box-sizing: border-box;
 
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(3, 1fr);
-  grid-column-gap: 0px;
-  grid-row-gap: 0px;
-} 
-
-.name{
-  grid-area: 1 / 1 / 2 / 3;
-  background-color: rgba(255, 255, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
 }
 
-.edit{
-  grid-area: 2 / 1 / 4 / 2;
-  background-color: rgba(90, 0, 255, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.name {
+    grid-area: 1 / 1 / 2 / 3;
+    background-color: rgba(255, 255, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
-.mute{
-  grid-area: 2 / 2 / 3 / 3;
-  background-color: rgba(255, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.edit {
+    grid-area: 2 / 1 / 4 / 2;
+    background-color: rgba(90, 0, 255, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
-.play{
-  grid-area: 3 / 2 / 4 / 3;
-  background-color: rgba(0, 128, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.mute {
+    grid-area: 2 / 2 / 3 / 3;
+    background-color: rgba(255, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
-</style>
+
+.play {
+    grid-area: 3 / 2 / 4 / 3;
+    background-color: rgba(0, 128, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style> 
--- src/components/Track.vue
+++ src/components/Track.vue
@@ -1,93 +1,92 @@
 <template>
-    <div class="content">
-      <div id="trackInterface">
-        <div id="instruments">
-          <div class="instrument"></div>
-          <div class="instrument"></div>
-          <div class="instrument"></div>
-          <div class="instrument"></div>
-          <div class="instrument"></div>
-          <div class="instrument"></div>
-        </div>
-        <div id="grids">
-          <div class="grid" id="track1"></div>
-          <div class="grid" id="track2"></div>
-          <div class="grid" id="track3"></div>
-          <div class="grid" id="track4"></div>
-          <div class="grid" id="track5"></div>
-          <div class="grid" id="track6"></div>
-        </div>
+  <div class="content">
+    <div id="trackInterface">
+      <div id="instruments">
+        <div class="instrument"></div>
+        <div class="instrument"></div>
+        <div class="instrument"></div>
+        <div class="instrument"></div>
+        <div class="instrument"></div>
+        <div class="instrument"></div>
       </div>
-    </div>  
+      <div id="grids">
+        <div class="grid" id="track1"></div>
+        <div class="grid" id="track2"></div>
+        <div class="grid" id="track3"></div>
+        <div class="grid" id="track4"></div>
+        <div class="grid" id="track5"></div>
+        <div class="grid" id="track6"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+#trackInterface {
+  width: auto;
+  height: 100%;
+  justify-items: stretch;
+  display: flex;
+}
 
-  #trackInterface{
-    width: 100%;
-    height: 100%;
-    justify-items: stretch;
-    display: flex;
-  }
+#instruments {
+  width: calc(100%/9);
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
 
-  #instruments{
-    width: 10%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-  }
+.instrument {
+  height: calc(100%/6);
+  width: 100%;
+  border: 3px solid rgb(60, 255, 255);
+  box-sizing: border-box;
+}
 
-  .instrument{
-    height: calc(100%/6);
-    width: 100%;
-    border: 1px solid rgb(60, 255, 255);
-    box-sizing: border-box;
-  }
+#grids {
+  width: 90%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
 
-  #grids{
-    width: 90%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-  }
+.grid {
+  width: 100%;
+  height: calc(100%/6);
+  display: flex;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+}
 
-  .grid {
-    width: 100%;
-    height: calc(100%/6);
-    display: flex;
-    flex-wrap: wrap;
-    border: 1px solid #fff;
-    box-sizing: border-box;
-  }
 
-  .selected{
-    background-color: #fff;
-  }
 
+.selected {
+  background-color: #fff;
+}
 </style>
 
 
 <script>
-export default{
-  mounted(){
+export default {
+  mounted() {
     document.querySelectorAll(".grid").forEach(grid => {
-      for(let x=0; x<8;x++) {
+      for (let x = 0; x < 8; x++) {
         var cell = document.createElement('div');
         cell.className = "cell";
         grid.appendChild(cell);
-        }
-      }),
-    document.querySelectorAll(".cell").forEach(cell => {
-      cell.addEventListener("click", event => {
-        let parent = event.target.parentNode;
-        let index = Array.from(parent.children).indexOf(event.target);
-        event.target.classList.toggle("selected");
-        this.changeState(index, parent.id);
+      }
+    }),
+      document.querySelectorAll(".cell").forEach(cell => {
+        cell.addEventListener("click", event => {
+          let parent = event.target.parentNode;
+          let index = Array.from(parent.children).indexOf(event.target);
+          event.target.classList.toggle("selected");
+          this.changeState(index, parent.id);
+        })
       })
-    })
   },
-methods: {
-    changeState(index, parentID) {  
+  methods: {
+    changeState(index, parentID) {
       this.$tracks.get(parentID)[index] = !this.$tracks.get(parentID)[index];
     }
   }
