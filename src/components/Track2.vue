89a212d826a8357c78c18049e7bf4ceef78af2c1
moritz-di <moritz.zuest@stud.hslu.ch>
2023-04-13 18:27:59 +0200

    fontend styling and header

--- src/components/Track2.vue
+++ src/components/Track2.vue
@@ -1,56 +1,12 @@
 <template>
   <div class="content">
-    <h1>Track 2</h1>
-    <div id="backbutton" @click="updateView(0)"></div>
-    <div class="trackInterface">
-      <div class="instrument"></div>
-      <div class="grid">
-        <div class="cell" v-for="item in items" :id="item.id" :tile="item.tile" @click="updateClick($event, item.tile)">
-        </div>
+    <div class="grid">
+      <div class="cell" v-for="item in items" :id="item.id" :tile="item.tile" @click="updateClick($event, item.tile)">
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-.trackInterface {
-  width: 100%;
-  height: 100%;
-  justify-items: stretch;
-  display: flex;
-}
-
-.instrument {
-  height: 10%;
-  width: calc(100%/9);
-  border: 3px solid rgb(60, 255, 255);
-  display: flex;
-  box-sizing: border-box;
-}
-
-h1 {
-  width: 100%;
-  text-align: left;
-}
-
-.grid {
-  height: 10%;
-  width: 90%;
-  display: flex;
-  flex-wrap: wrap;
-  box-sizing: border-box;
-}
-
-.selected {
-  background-color: #fff;
-}
-
-#backbutton {
-  background-color: aqua;
-  height: 10%;
-  width: 10%;
-}
-</style>
 
 <script>
 import { socket } from '../client'
